e891d7b304e233599d68f4d01ded562953680ddd
gorretgolden <nabatanzigorret143@gmail.com>
2025-02-26 15:12:04 +0300

    added binary tree definition

---
+++ binary-trees/intro.py
@@ -0,0 +1,14 @@
+# Binary trees are used to store and manage hierarchical data efficiently. 
+# They provide a structured way to organize data where each element (node) has at
+# most two children (left and right).
+# A Binary Tree is a tree data structure where:
+
+# Each node has at most 2 children (left and right).
+# The topmost node is called the root.
+# If a node has no children, it’s called a leaf node.
+
+#        A
+#       / \
+#      B   C
+#     / \   \
+#    D   E   F
